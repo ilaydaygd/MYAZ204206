42539ba16aea5aaf00c5d1354c2b93fe8186ed08
ilaydaygd <ilaydaygd@gmail.com>
2023-05-13 02:21:00 +0300

    apps

---
+++ DataStructuresAlgorithms/BigOWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BigOWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
