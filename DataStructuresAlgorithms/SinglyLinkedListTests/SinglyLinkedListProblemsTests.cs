0d3056db25eabb2bd5884c715ebf9842a803089b
ilaydaygd <ilaydaygd@gmail.com>
2023-05-14 00:57:57 +0300

    14.05.23

---
+++ DataStructuresAlgorithms/SinglyLinkedListTests/SinglyLinkedListProblemsTests.cs
@@ -0,0 +1,12 @@
+﻿namespace SinglyLinkedListTests
+{
+    public class SinglyLinkedListProblemsTests
+    {
+        [Fact]
+        public void SinglyLinkedList_Concatenate_Test()
+        {
+            //throw new NotImplementedException();
+            Assert.Equal(0, 0);
+        }
+    }
+}
---
+++ DataStructuresAlgorithms/SortingAlgorithmsTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace SortingAlgorithmsTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
